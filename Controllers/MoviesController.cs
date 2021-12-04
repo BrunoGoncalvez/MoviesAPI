2d6192c2b75128bb78cda334be2f184ee24f48cb
Matheus S. Gonçalves <matheus-x4@hotmail.com>
2021-12-04 17:45:13 -0300

    Exclusão de Controller exemploe criação do controller movies

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Personal.Movies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+    }
+}
